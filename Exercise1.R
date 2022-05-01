0c3df84029d3fe49a0f59b002211d650b4d87383
mario carrillo <carrillo@rhrk.uni-kl.de>
2022-05-01 12:25:56 +0200

    Adjust parameters to save results
    This commit adds a couple of customizations to save the results
    in png images.
    
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

--- Exercise1.R
+++ Exercise1.R
@@ -1,47 +1,53 @@
-#Yu-Chi Huang
+# Yu-Chi Huang
 # Mario Carrillo
 
-roll = function(n){
+# Clean env
+rm(list=ls())
+
+roll <- function(N) {
   coin <- c('heads', 'tails')
-  result<-sample(coin, size = n, replace = TRUE)
+  result<-sample(coin, size = N, replace = TRUE)
 
   return(table(result)/length(result))
 }
 
-simulation= function(){
+# Simulate flip coin
+simulation <- function(e) {
   times <- seq(10, 200, 10)
-  #times <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
   frequency_list<-list()
-  f<- c()
+  f <- c()
 
-  for(i in times){
-    relative_frequency<-roll(i)
-    
+  for(i in times) {
+    relative_frequency <- roll(i)
     writeLines('\nrelative frequency')
     cat(i, 'times\n')
     print(relative_frequency)
-
-    frequency_list<-c(frequency_list, relative_frequency["heads"])
+    frequency_list <- c(frequency_list, relative_frequency["heads"])
   }
-  
-  writeLines('\nthe relative frequency of 6 against N')
-  t<-10
-  for (k in frequency_list){
+
+  writeLines('\nThe relative frequency of heads against N')
+  t <- 10
+  for(k in frequency_list) {
     cat(t, ' times: ',k, '\n')
-    t<-t+10
-    f<-c(f, k)
+    t <- t + 10
+    f <- c(f, k)
   }
 
-  my_bar<-barplot(f, xlab = 'times', ylab = ' relative frequency', main = 'the relative frequency of heads against N', ylim=c(0, 1), names.arg=times)
-  text(my_bar, f+0.03, round(f, digits = 2))
-  #plot(times, frequency_list, xlab = 'times', ylab = ' relative frequency', main = 'the relative frequency of 6 against N', ylim=range(0,1), type = "o")
+  out_img <- sprintf("E1_experiment_%d.png", e)
+  png(file=out_img,  width=800, height=400, pointsize=10,)
+  my_bar<-barplot(f,
+                  xlab = 'N times',
+                  ylab = 'Relative frequency',
+                  main = 'The relative frequency of heads against N',
+                  ylim=c(0, 1),
+                  col = rgb(0.2,0.4,0.6,0.6),
+                  names.arg=times)
+  text(my_bar, f + 0.03, round(f, digits = 2))
+  dev.off()
 }
 
-j=0
-repeat{
-  j=j+1
-  cat('\n\nROUND ',j)
-  simulation()
-  if(j==3)
-    break
+# Repeat experiment 3 Times
+for(e in 1:3) {
+  simulation(e)
+  print(sprintf("Experiment: %d", e))
 }
