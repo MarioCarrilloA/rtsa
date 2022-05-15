47f6f9d1a01062c8365fe559a6c1ac471d6ce2b6
mario carrillo <carrillo@rhrk.uni-kl.de>
2022-05-15 10:46:24 +0200

    Add Exercise03 part 4
    Linear regression analysis
    
    Signed-off-by: mario carrillo <carrillo@rhrk.uni-kl.de>

---
+++ Exercise03.4.R
@@ -0,0 +1,66 @@
+# Yu-Chi Huang
+# Mario Carrillo
+
+# Clean environment
+rm(list=ls())
+
+
+make_plots <- function(id) {
+  # Load data
+  filename <- sprintf("data%d.txt", id)
+  data <- read.table(filename, sep=",")
+  x <- data$V1
+  Y <- data$V2
+
+  # Fit model
+  a <- lm(Y~x)
+
+  # Create output directory
+  out_dir <- "./outs"
+  dir.create(file.path(out_dir), showWarnings = FALSE)
+
+  # Out PDF
+  out_pdf <- sprintf("%s/data%d.pdf", out_dir, id)
+  pdf(file=out_pdf, width=12, height=8)
+
+  # Out PNG image (NOTE: At the moment PDF and PNG cannot work
+  # at the same time. Therefore, if you uncomment this part, it
+  # is necessary to comment PDF part).
+  #
+  #out_img <- sprintf("%s/data%d.png", out_dir, id)
+  #png(file=out_img, width=1200, height=800, pointsize=20)
+
+  # Plot the 4 charts in a 2x2 grid
+  par(mfrow=c(2,2))
+
+  # Scatter plot
+  plot(x, Y, xlab="x", ylab="Y", main="Scatter Plot")
+  t<-seq(, 1, 0.1)
+  abline(a, col="red")
+
+  # Residual plot
+  plot(predict(a),
+       a$residuals,
+       main="Resiudal plot",
+       xlab="predicted values",
+       ylab="residuals")
+  lines(c(min(predict(a)-10), max(predict(a)+10)), c(0,0), col="green")
+
+  # Q-Q Plot
+  plot(a, c(2, 2))
+
+  # Hat matrix vs squared residuals
+  plot(a$residuals^2,
+       lm.influence(a)$hat,
+       main="Squared residuals vs Hii",
+       xlab="Residuals^2",
+       ylab="Leverage points (Hii)",
+       xlim = c(min(a$residuals^2), max(a$residuals^2)),
+       ylim = c(min(lm.influence(a)$hat), max(lm.influence(a)$hat)))
+  dev.off()
+}
+
+for(id in seq(1:5)) {
+  make_plots(id)
+}
+
