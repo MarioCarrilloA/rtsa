fede5b056703b07896e324aeb59d132c27282831
Y-C <43612896+gvuiiotlaar@users.noreply.github.com>
2022-06-14 00:04:55 +0200

    Adjusted R^2, Residual Mean Square, Mallow's Cp

---
+++ Exercise7.3.R
@@ -0,0 +1,34 @@
+#Yu-Chi Huang
+#Mario Carrillo
+
+all_subset_regression = function(data){
+    models_names <- getmodels('count', c('date', 'temp', 'atemp', 'humidity', 'windspeed', 'registered'))
+    N = length(data$count)
+    #get largest model for Mallow's Cp
+    fit_largest = lm(models_names[length(models_names)], data = data)
+    d = length(fit_largest$coefficients)-1
+    s_hat_square_max = sum(residuals(fit_largest)^2)*(1/(N-d))
+    adjusted_R_square = array(dim=(length(models_names)))
+    residual_mean_square = array(dim=(length(models_names)))
+    Mallows_Cp = array(dim=(length(models_names)))
+    for(i in 1:length(models_names)){
+      fit = lm(models_names[i], data = data)
+      d = length(fit$coefficients)-1
+      residual_sum_squares = sum(residuals(fit)^2)
+      total_sum_squares = sum(data$count-mean(data$count)^2)
+      adjusted_R_square[i] = 1-((residual_sum_squares/(N-d))/(total_sum_squares/(N-1)))
+      residual_mean_square[i] =  residual_sum_squares/(N-d)
+      Mallows_Cp[i] = residual_sum_squares/s_hat_square_max-(N-2*d)
+    }
+    print(adjusted_R_square)
+    cat('Adjusted R^2: ', max(adjusted_R_square), ', ', models_names[which.max(adjusted_R_square)])
+    cat('\nResidual Mean Square: ', min(residual_mean_square), ', ', models_names[which.min(residual_mean_square)])
+    cat('\nMallow\'s Cp: ', min(Mallows_Cp), ', ', models_names[which.min(Mallows_Cp)])
+}
+
+data = read.csv("E-Scooters.csv")
+data$date=as.numeric(as.POSIXct(data$date, format = "%Y-%m-%d"))/24/60/60
+
+source('getmodels.r')
+
+all_subset_regression(data)
